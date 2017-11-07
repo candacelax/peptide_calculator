c2cdeea80e8d06c0627ccf50bc346a5fab12e1b9
Candace Ross <candacelax@gmail.com>
2017-11-06 22:26:39 -0500

    updated help statements

--- side_product_calculator.py
+++ side_product_calculator.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 
-# command to run
-# confused? run ./side_product_calculator.py --help
-
-# python SCRIPT_NAME ARGS..
-# python side_product_calculator.py --seq 'AAAAA' --fin FILE_IN --fout FILE_OUT
-
 import argparse
-import re
 from mechanize import Browser
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--fin', type=str, dest='file_in',
-                        help='input file TODO')
+                        help='file containing list of input files')
     parser.add_argument('--seq', type=str, dest='sequence',
-                        help='input file TODO')
-    parser.add_argument('--fout', type=str, dest='file_out')
+                        help='amino acid sequence (note: use one letter abbreviations)')
+    parser.add_argument('--fout', type=str, dest='file_out',
+                        help='output file for products')
     args = parser.parse_args()
     return args
 
@@ -24,8 +18,7 @@ def get_products(sequence, file_in):
     # open site and get form
     br = Browser()
     br.open("http://peptide.alexmijalis.com/")
-    br.select_form(nr=0)
-
+    br.select_form(nr=0) # there's only a single form
 
     # get response
     br['text'] = sequence
@@ -40,19 +33,11 @@ def get_products(sequence, file_in):
 
 def write_output_to_file(file_out, product_list):
     with open(file_out, 'w') as f:
-        for line in product_list:
-            f.write(line + '\n')
+        map(lambda l: f.write(l + 'n'),
+            product_list)
 
 
 if __name__ == '__main__':
     args = parse_args()
-
     products = get_products(args.sequence, args.file_in)
     write_output_to_file(args.file_out, products)
-
-
-    # for list of input, output files
-    # with open(file_in, 'r') as f:
-    #     for filename in f:
-    #         products = get-prof(file)
-    #         write(prod)
